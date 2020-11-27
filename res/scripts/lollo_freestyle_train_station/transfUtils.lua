5ecfd85aea6595f7daedb81f863a1985826c7055
lolluslollus <lolluslollus@outlook.com>
2020-11-27 22:21:08 +0100

    tracks transf fixed

--- res/scripts/lollo_freestyle_train_station/transfUtils.lua
+++ res/scripts/lollo_freestyle_train_station/transfUtils.lua
@@ -131,18 +131,11 @@ utils.getVec123Transformed = function(vec123, transf)
 end
 
 utils.getPosTanX2Transformed = function(posTanX2, transf)
-    -- LOLLO TODO also the points need rotating
     local point1 = {posTanX2[1][1][1], posTanX2[1][1][2], posTanX2[1][1][3]}
     local point2 = {posTanX2[2][1][1], posTanX2[2][1][2], posTanX2[2][1][3]}
     local tan1 = {posTanX2[1][2][1], posTanX2[1][2][2], posTanX2[1][2][3]}
     local tan2 = {posTanX2[2][2][1], posTanX2[2][2][2], posTanX2[2][2][3]}
 
-    local traslTransf = {
-        1, 0, 0, 0,
-        0, 1, 0, 0,
-        0, 0, 1, 0,
-        transf[13], transf[14], transf[15], 1
-    }
     local rotateTransf = {
         transf[1], transf[2], transf[3], transf[4],
         transf[5], transf[6], transf[7], transf[8],
@@ -152,11 +145,11 @@ utils.getPosTanX2Transformed = function(posTanX2, transf)
 
     local result = {
         {
-            utils.getVec123Transformed(point1, traslTransf),
+            utils.getVec123Transformed(point1, transf),
             utils.getVec123Transformed(tan1, rotateTransf)
         },
         {
-            utils.getVec123Transformed(point2, traslTransf),
+            utils.getVec123Transformed(point2, transf),
             utils.getVec123Transformed(tan2, rotateTransf)
         }
     }
