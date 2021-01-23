5ee491c253bae6cded6455864a43e882ac222bcc
lolluslollus <lolluslollus@outlook.com>
2021-01-23 11:25:35 +0100

    finishing touch for second upgrade

--- mod.lua
+++ mod.lua
@@ -3,7 +3,7 @@ function data()
 
     return {
         info = {
-            minorVersion = 1,
+            minorVersion = 2,
             severityAdd = 'NONE',
             severityRemove = 'WARNING',
             name = _('NAME'),
