46dc016c8c45292f42d2ec9983cd9623ac19f5d6
lolluslollus <lolluslollus@outlook.com>
2021-02-01 22:07:58 +0100

    bump version

--- mod.lua
+++ mod.lua
@@ -3,7 +3,7 @@ function data()
 
     return {
         info = {
-            minorVersion = 13,
+            minorVersion = 14,
             severityAdd = 'NONE',
             severityRemove = 'WARNING',
             name = _('NAME'),
