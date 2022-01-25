03ef219be8496b04d732259ee1e37bb5fb5d38f3
lolluslollus <lolluslollus@users.noreply.github.com>
2022-01-25 11:03:31 +0100

    Create lollo_1m_path_concrete_country.lua

---
+++ res/config/street/lollo_1m_path_concrete_country.lua
@@ -0,0 +1,51 @@
+function data()
+    return {
+        numLanes = 2,
+        -- transportModesStreet = {'PERSON'}, -- dumps
+        -- transportModesStreet = {'TRUCK'}, -- it dumps with an empty array or with {'PERSON'}; savegames dump with {'TRUCK'}
+        sidewalkWidth = 0.4,
+        sidewalkHeight = 0.0,
+        streetWidth = 0.2,
+        yearFrom = game.config.busLaneYearFrom,
+        yearTo = 0,
+        aiLock = true,
+        visibility = true,
+        country = true,
+        speed = 20.0,
+        type = 'lollo_1m_path_concrete_country',
+        name = _("1 Metre Concrete Path, No Buildings"),
+        desc = _("1 metre concrete path with a speed limit of %2% and no buildings, give it a bus lane to pedestrianise it."),
+        categories = { 'paths' },
+        order = 0,
+        busAndTramRight = true,
+        -- slopeBuildSteps = 1,
+        embankmentSlopeLow  = 0.75,
+        embankmentSlopeHigh  = 2.5,
+        borderGroundTex = "street_border.lua",
+        materials = {
+            -- sidewalkBorderOuter = {
+            --     name = "street/old_medium_sidewalk_border_outer.mtl",
+            --     -- size = { 16.0, 0.41602 }
+            --     size = { 16.0, 0.2 }
+            -- },
+            sidewalkPaving = {
+                -- name = "street/old_medium_sidewalk.mtl",
+                -- name = "lollo_freestyle_train_station/bridge/cement_no_pattern.mtl",
+                -- name = "lollo_freestyle_train_station/bridge/lighter_cement_no_pattern.mtl",
+                -- name = "lollo_freestyle_train_station/bridge/cement_shrunk.mtl",
+                name = "lollo_freestyle_train_station/station_concrete_1_low_prio.mtl",
+                size = { 4.0, 4.0 }
+            },
+            streetPaving = {
+                -- name = "street/old_medium_sidewalk.mtl",
+                -- name = "lollo_freestyle_train_station/bridge/cement_no_pattern.mtl",
+                -- name = "lollo_freestyle_train_station/bridge/lighter_cement_no_pattern.mtl",
+                -- name = "lollo_freestyle_train_station/bridge/cement_shrunk.mtl",
+                name = "lollo_freestyle_train_station/station_concrete_1_low_prio.mtl",
+                size = { 4.0, 4.0 }
+            },
+        },
+        assets = {},
+        cost = 1.0
+    }
+end
