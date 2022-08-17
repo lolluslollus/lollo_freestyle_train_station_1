f09cfc7497a2b92e8d26ecbc6496572361d501c8
lolluslollus <lolluslollus@users.noreply.github.com>
2022-08-17 17:37:37 +0200

    unbump and clean

--- mod.lua
+++ mod.lua
@@ -5,7 +5,7 @@ function data()
 
     return {
         info = {
-            minorVersion = 74,
+            minorVersion = 73,
             severityAdd = 'NONE',
             severityRemove = 'WARNING',
             name = _('NAME'),
