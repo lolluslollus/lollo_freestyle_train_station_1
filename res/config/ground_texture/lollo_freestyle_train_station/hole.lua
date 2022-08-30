01d22b83301385f2fad81df2871e174894643c02
lolluslollus <lolluslollus@users.noreply.github.com>
2022-08-30 21:05:28 +0200

    comments, fruitless attempt with hole.lua and prio

--- res/config/ground_texture/lollo_freestyle_train_station/hole.lua
+++ res/config/ground_texture/lollo_freestyle_train_station/hole.lua
@@ -1,18 +1,34 @@
-local tu = require "texutil"
+-- local tu = require "texutil"
 -- LOLLO TODO tried these to see how they handle the hole. they all suck the same.
+-- function data()
+-- return {
+-- 	-- texture = tu.makeTextureLinearNearest("res/textures/terrain/material/mat255.tga", true, false,false),
+-- 	-- texture = tu.makeTextureLinearClamp("res/textures/terrain/material/mat255.tga", true, false,false),
+-- 	texture = tu.makeTextureMipmapRepeat("res/textures/terrain/material/mat255.tga", true, false,false),
+-- 	-- texture = tu.makeTextureMipmapClamp("res/textures/terrain/material/mat255.tga", true, false,false),
+-- 	-- texture = tu.makeTextureMipmapClampVertical("res/textures/terrain/material/mat255.tga", true, false,false),
+-- 	-- texSize = { 2.0, 2.0 },
+-- 	-- texSize = { 1.0, 1.0 },
+-- 		texSize = { 32.0, 32.0 },
+-- 	-- texSize = { 64.0, 64.0 },
+-- 	materialIndexMap = {
+-- 		[255] = "",
+-- 	},
+-- 	priority = 999999999
+-- }
+-- end
+
+-- original with changed priority
+local tu = require "texutil"
+
 function data()
 return {
-	-- texture = tu.makeTextureLinearNearest("res/textures/terrain/material/mat255.tga", true, false,false),
-	-- texture = tu.makeTextureLinearClamp("res/textures/terrain/material/mat255.tga", true, false,false),
-	texture = tu.makeTextureMipmapRepeat("res/textures/terrain/material/mat255.tga", true, false,false),
-	-- texture = tu.makeTextureMipmapClamp("res/textures/terrain/material/mat255.tga", true, false,false),
-	-- texture = tu.makeTextureMipmapClampVertical("res/textures/terrain/material/mat255.tga", true, false,false),
-	-- texSize = { 2.0, 2.0 },
-	-- texSize = { 1.0, 1.0 },
-	texSize = { 64.0, 64.0 },
+	texture = tu.makeTextureLinearNearest("res/textures/terrain/material/mat255.tga", true, false,false),
+	texSize = { 32.0, 32.0 },
 	materialIndexMap = {
 		[255] = "",
 	},
-	priority = 15000
+	priority = 999999999999999999
 }
 end
+
