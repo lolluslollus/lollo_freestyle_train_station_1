6a96605e6c5ea785045b5d7ea69617342c1027ee
lolluslollus <lolluslollus@users.noreply.github.com>
2022-09-13 18:25:04 +0200

    restore correct minor version

--- mod.lua
+++ mod.lua
@@ -6,7 +6,7 @@ function data()
 
     return {
         info = {
-            minorVersion = 86,
+            minorVersion = 84,
             severityAdd = 'NONE',
             severityRemove = 'WARNING',
             name = _('NAME'),
