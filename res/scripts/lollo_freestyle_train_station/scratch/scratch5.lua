5fe9c35a427bc06a5dce8bdd8037bbbbee772c7a
lolluslollus <lolluslollus@users.noreply.github.com>
2022-09-14 11:35:24 +0200

    Update scratch5.lua

--- res/scripts/lollo_freestyle_train_station/scratch/scratch5.lua
+++ res/scripts/lollo_freestyle_train_station/scratch/scratch5.lua
@@ -4,7 +4,7 @@ local transfUtils = require('lollo_freestyle_train_station.transfUtils')
 
 local vec1 = { 60289776611328, 602.89776611328, 0.00060289776611328, 99.9, 99.99, 100.0, 100.0, -0.00012345}
 local vec2 = { 60296420214167, 602.96420214167, 0.00060296420214167, 100.0, 100.0, 100.1, 100.01, 0.00012345}
--- print(os.time())
+-- print('mSec =', os.clock() * 1000)
     local a, b = transfUtils.isNumVeryClose(vec1[1], vec2[1], 3)
     local aa, bb = transfUtils.isNumVeryClose(vec1[2], vec2[2], 3)
     local aaa, bbb = transfUtils.isNumVeryClose(vec1[3], vec2[3], 3)
@@ -13,7 +13,7 @@ local vec2 = { 60296420214167, 602.96420214167, 0.00060296420214167, 100.0, 100.
     local aaaaaa, bbbbbb = transfUtils.isNumVeryClose(vec1[6], vec2[6], 3) -- same
     local aaaaaaa, bbbbbbb = transfUtils.isNumVeryClose(vec1[7], vec2[7], 3) -- same
     local aaaaaaaa, bbbbbbbb = transfUtils.isNumVeryClose(vec1[8], vec2[8], 3)
--- print(os.time()) -- 1661804180 - 1661804188 with the string format
+-- print('mSec =', os.clock() * 1000) -- 1661804180 - 1661804188 with the string format
 local results = {}
 for i = 1, 8 do
     results[i] = transfUtils.isNumsCloserThan(vec1[i], vec2[i], 0.001)
@@ -21,7 +21,7 @@ end
 
 local vec1M = { -60289776611328, -602.89776611328, -0.00060289776611328, -99.9, -99.99, -100.0, -100.0, 0.00012345}
 local vec2M = { -60296420214167, -602.96420214167, -0.00060296420214167, -100.0, -100.0, -100.1, -100.01, -0.00012345}
--- print(os.time())
+-- print('mSec =', os.clock() * 1000)
     local aM, bM = transfUtils.isNumVeryClose(vec1M[1], vec2M[1], 3)
     local aaM, bbM = transfUtils.isNumVeryClose(vec1M[2], vec2M[2], 3)
     local aaaM, bbbM = transfUtils.isNumVeryClose(vec1M[3], vec2M[3], 3)
