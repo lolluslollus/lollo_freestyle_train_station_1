e344ff160a7b31eee331d7b8a6b0eebf00e55cfd
lolluslollus <lolluslollus@users.noreply.github.com>
2023-02-24 10:13:11 +0100

    wall assets: also skew the base wall + bump

--- mod.lua
+++ mod.lua
@@ -6,7 +6,7 @@ function data()
 
     return {
         info = {
-            minorVersion = 107,
+            minorVersion = 108,
             severityAdd = 'NONE',
             severityRemove = 'WARNING',
             name = _('NAME'),
