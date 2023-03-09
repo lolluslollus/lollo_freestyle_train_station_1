ec995cfe871025f667d631fddeef0c0a7dcfdfe3
lolluslollus <lolluslollus@users.noreply.github.com>
2023-03-09 18:14:35 +0100

    fix openLiftFree_v2.con and bump

--- mod.lua
+++ mod.lua
@@ -6,7 +6,7 @@ function data()
 
     return {
         info = {
-            minorVersion = 114,
+            minorVersion = 115,
             severityAdd = 'NONE',
             severityRemove = 'WARNING',
             name = _('NAME'),
