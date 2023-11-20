7e17ffb8db850980bc2a799e792a6e66dd59e407
lolluslollus <lolluslollus@users.noreply.github.com>
2023-11-20 11:05:51 +0100

    bump

--- mod.lua
+++ mod.lua
@@ -6,7 +6,7 @@ function data()
 
     return {
         info = {
-            minorVersion = 124,
+            minorVersion = 125,
             severityAdd = 'NONE',
             severityRemove = 'WARNING',
             name = _('NAME'),
